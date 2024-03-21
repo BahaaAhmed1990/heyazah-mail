f5b10c7c5007443a1369067577bbb14861e42728
unknown <bahaaahmed19902012@gmail.com>
2024-03-21 02:04:01 +0200

    set cors to pass heyazah domain

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const port = process.env.PORT || 4222;
 const app = express();
 
 console.log(__dirname);
-
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://heyazah.cloud");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
